323e2befafe76caa78ec8c384f3b444776bae657
Robbie Campbell <rcampbell182@gmail.com>
2016-08-08 16:24:06 +0100

    added address route to controller

---
+++ controller.rb
@@ -0,0 +1,20 @@
+require( "sinatra" )
+require( "sinatra/contrib/all" )
+require( "json" )
+
+require_relative( "./models/word_formatter" )
+
+get "/" do
+  erb( :home )
+end
+
+get "/address" do
+  content_type( :json )
+  address = {
+    address: "17 Crown Drive",
+    buuilding: "Roodlands",
+    postcode: "iv2 3nn",
+    phone: "01463 241 546"
+  }
+  return address.to_json()
+end
